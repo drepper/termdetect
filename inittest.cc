03e2c41b9f842a0c00f88474ec1e4adb6d6da0a7
Ulrich Drepper <drepper@gmail.com>
2024-04-16 15:35:35 +0200

    Implement remaining features

--- inittest.cc
+++ inittest.cc
@@ -10,5 +10,11 @@ int main()
   std::cout << "implementation         = " << ti->implementation_name() << std::endl;
   std::cout << "implementation version = " << ti->implementation_version << std::endl;
   std::cout << "emulation              = " << ti->emulation_name() << std::endl;
+  std::cout << "features               =";
+  for (auto f : ti->feature_set)
+    std::cout << ' ' << ti->feature_name(f);
+  if (! ti->unknown_features.empty())
+    std::cout << ' ' << ti->unknown_features;
+  std::cout << std::endl;
   std::cout << "raw                    = " << ti->raw << std::endl;
 }
