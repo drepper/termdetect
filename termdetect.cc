2c338441055d0102c9a93c008873600f109faa78
Ulrich Drepper <drepper@gmail.com>
2024-05-14 16:06:43 +0200

    💡 Add tilix as VTE-based emulator

--- termdetect.cc
+++ termdetect.cc
@@ -533,7 +533,7 @@ namespace terminal {
       // +----------------+-----------+---------------+-----------+-----------+-----------+------------+
       //
       // Other terminals use the same engines:
-      // VTE: gnome-console, mate-terminal, lxterminal, xfce4-terminal, roxterm
+      // VTE: gnome-console, mate-terminal, lxterminal, xfce4-terminal, roxterm, tilix
       // QT5: deepin, qterminal
 
       // We are desperate when checking for eterm and emacs term.  They do not handle any request and others than
