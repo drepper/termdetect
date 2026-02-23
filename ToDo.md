005d4359ab37733a0bdecc54f282bec22c4e0eab
Ulrich Drepper <github@akkadia.org>
2026-02-23 14:23:03 +0100

    🚧 More feature flags, unused for now

--- ToDo.md
+++ ToDo.md
@@ -1,1 +1,229 @@
 - [ ] OSC52: set clipboard with escape sequence.  Disabled often for security reasons.  Check how to check at runtime
+
+# From blessed
+
+See [repo](https://github.com/jquast/blessed/blob/master/blessed/dec_modes.py)
+
+- [ ] Use the DEC modes for the features
+- [ ] not bit values, add interface to query specific features
+- [ ] add caching of all requests, there should not be a second test
+
+## Generalized query?
+
+in `terminal.py` from blessed, the `get_dec_mode` function uses a query
+
+```Python
+f'\x1b[?{int(mode):d}$p'
+```
+
+and an expected result in the form of
+
+```Python
+re.compile(f'\x1b\\[\\?{int(mode):d};([0-4])\\$y')
+```
+
+The value returned after the semicolon is one of
+- '0': not recognized
+- '1': set
+- '2': reset
+- '3': permanenty set
+- '4': permanenty reset
+
+Check whether this applies to all emulators.  Notes from jqast's site:
+
+- Terminology is inconsistent (corruptions?)
+- iTerm2 has many "supported, but disabled, and cannot be changed"
+- Konsole does not reply to these queries
+- Contour returns sometimes replies with a different mode number (query: 25 -> reply: 80)
+
+
+## Known modes
+
+```
+    DECCKM = 1
+    DECANM = 2
+    DECCOLM = 3  # https://vt100.net/docs/vt510-rm/DECCOLM.html
+    DECSCLM = 4  # https://vt100.net/docs/vt510-rm/DECSCLM.html
+    DECSCNM = 5  # https://vt100.net/docs/vt510-rm/DECSCNM.html
+    DECOM = 6    # https://vt100.net/docs/vt510-rm/DECOM.html
+    DECAWM = 7   # https://vt100.net/docs/vt510-rm/DECAWM.html
+    DECARM = 8   # https://vt100.net/docs/vt510-rm/DECARM.html
+    DECINLM = 9
+    DECEDM = 10
+    DECLTM = 11
+    DECKANAM = 12
+    DECSCFDM = 13
+    DECTEM = 14
+    DECEKEM = 16
+    DECPFF = 18
+    DECPEX = 19
+    OV1 = 20
+    BA1 = 21
+    BA2 = 22
+    PK1 = 23
+    AH1 = 24
+    DECTCEM = 25
+    DECPSP = 27
+    DECPSM = 29
+    SHOW_SCROLLBAR_RXVT = 30
+    DECRLM = 34
+    DECHEBM = 35
+    DECHEM = 36
+    DECTEK = 38
+    DECCRNLM = 40
+    DECUPM = 41
+    DECNRCM = 42
+    DECGEPM = 43
+    DECGPCM = 44
+    DECGPCS = 45
+    DECGPBM = 46
+    DECGRPM = 47
+    DECTHAIM = 49
+    DECTHAICM = 50
+    DECBWRM = 51
+    DECOPM = 52
+    DEC131TM = 53
+    DECBPM = 55
+    DECNAKB = 57
+    DECIPEM = 58
+    DECKKDM = 59
+    DECHCCM = 60
+    DECVCCM = 61
+    DECPCCM = 64
+    DECBCMM = 65
+    DECNKM = 66
+    DECBKM = 67
+    DECKBUM = 68
+    DECVSSM = 69
+    DECFPM = 70
+    DECXRLM = 73
+    DECSDM = 80
+    DECKPM = 81
+    WY_52_LINE = 83
+    WYENAT_OFF = 84
+    REPLACEMENT_CHAR_COLOR = 85
+    DECTHAISCM = 90
+    DECNCSM = 95
+    DECRLCM = 96
+    DECCRTSM = 97
+    DECARSM = 98
+    DECMCM = 99
+    DECAAM = 100
+    DECCANSM = 101
+    DECNULM = 102
+    DECHDPXM = 103
+    DECESKM = 104
+    DECOSCNM = 106
+    DECNUMLK = 108
+    DECCAPSLK = 109
+    DECKLHIM = 110
+    DECFWM = 111
+    DECRPL = 112
+    DECHWUM = 113
+    DECATCUM = 114
+    DECATCBM = 115
+    DECBBSM = 116
+    DECECM = 117
+
+    # Mouse reporting modes and xterm/rxvt extensions
+    MOUSE_REPORT_CLICK = 1000
+    MOUSE_HILITE_TRACKING = 1001
+    MOUSE_REPORT_DRAG = 1002
+    MOUSE_ALL_MOTION = 1003
+    FOCUS_IN_OUT_EVENTS = 1004
+    MOUSE_EXTENDED_UTF8 = 1005
+    MOUSE_EXTENDED_SGR = 1006
+    ALT_SCROLL_XTERM = 1007
+    SCROLL_ON_TTY_OUTPUT_RXVT = 1010
+    SCROLL_ON_KEYPRESS_RXVT = 1011
+    FAST_SCROLL = 1014
+    MOUSE_URXVT = 1015
+    MOUSE_SGR_PIXELS = 1016
+    BOLD_ITALIC_HIGH_INTENSITY = 1021
+
+    # Keyboard and meta key handling modes
+    META_SETS_EIGHTH_BIT = 1034
+    MODIFIERS_ALT_NUMLOCK = 1035
+    META_SENDS_ESC = 1036
+    KP_DELETE_SENDS_DEL = 1037
+    ALT_SENDS_ESC = 1039
+
+    # Selection, clipboard, and window manager hint modes
+    KEEP_SELECTION_NO_HILITE = 1040
+    USE_CLIPBOARD_SELECTION = 1041
+    URGENCY_ON_CTRL_G = 1042
+    RAISE_ON_CTRL_G = 1043
+    REUSE_CLIPBOARD_DATA = 1044
+    EXTENDED_REVERSE_WRAPAROUND = 1045
+    ALT_SCREEN_BUFFER_SWITCH = 1046
+
+    # Alternate screen buffer and cursor save/restore combinations
+    ALT_SCREEN_BUFFER_XTERM = 1047
+    SAVE_CURSOR_DECSC = 1048
+    ALT_SCREEN_AND_SAVE_CLEAR = 1049
+
+    # Terminal info and function key emulation modes
+    TERMINFO_FUNC_KEY_MODE = 1050
+    SUN_FUNC_KEY_MODE = 1051
+    HP_FUNC_KEY_MODE = 1052
+    SCO_FUNC_KEY_MODE = 1053
+
+    # Legacy keyboard emulation modes
+    LEGACY_KBD_X11R6 = 1060
+    VT220_KBD_EMULATION = 1061
+
+    SIXEL_PRIVATE_PALETTE = 1070
+
+    # VTE BiDi extensions
+    BIDI_ARROW_KEY_SWAPPING = 1243
+
+    # iTerm2 extensions
+    ITERM2_REPORT_KEY_UP = 1337
+
+    # XTerm readline and mouse enhancements
+    READLINE_MOUSE_BUTTON_1 = 2001
+    READLINE_MOUSE_BUTTON_2 = 2002
+    READLINE_MOUSE_BUTTON_3 = 2003
+    BRACKETED_PASTE = 2004
+    READLINE_CHARACTER_QUOTING = 2005
+    READLINE_NEWLINE_PASTING = 2006
+
+    # Modern terminal extensions
+    SYNCHRONIZED_OUTPUT = 2026
+    GRAPHEME_CLUSTERING = 2027
+    TEXT_REFLOW = 2028
+    PASSIVE_MOUSE_TRACKING = 2029
+    REPORT_GRID_CELL_SELECTION = 2030
+    COLOR_PALETTE_UPDATES = 2031
+    IN_BAND_WINDOW_RESIZE = 2048
+
+    # VTE bidirectional text extensions
+    MIRROR_BOX_DRAWING = 2500
+    BIDI_AUTODETECTION = 2501
+
+    # mintty extensions
+    AMBIGUOUS_WIDTH_REPORTING = 7700
+    SCROLL_MARKERS = 7711
+    REWRAP_ON_RESIZE_MINTTY = 7723
+    APPLICATION_ESCAPE_KEY = 7727
+    ESC_KEY_SENDS_BACKSLASH = 7728
+    GRAPHICS_POSITION = 7730
+    ALT_MODIFIED_MOUSEWHEEL = 7765
+    SHOW_HIDE_SCROLLBAR = 7766
+    FONT_CHANGE_REPORTING = 7767
+    GRAPHICS_POSITION_2 = 7780
+    SHORTCUT_KEY_MODE = 7783
+    MOUSEWHEEL_REPORTING = 7786
+    APPLICATION_MOUSEWHEEL = 7787
+    BIDI_CURRENT_LINE = 7796
+
+    # Terminal-specific extensions
+    TTCTH = 8200
+    SIXEL_SCROLLING_LEAVES_CURSOR = 8452
+    CHARACTER_MAPPING_SERVICE = 8800
+    AMBIGUOUS_WIDTH_DOUBLE_WIDTH = 8840
+    WIN32_INPUT_MODE = 9001
+    KITTY_HANDLE_CTRL_C_Z = 19997
+    MINTTY_BIDI = 77096
+    INPUT_METHOD_EDITOR = 737769
+```
--- termdetect.hh
+++ termdetect.hh
@@ -1,13 +1,13 @@
 #ifndef _TERMDETECT_HH
-#define _TERMDETECT_HH 1
+# define _TERMDETECT_HH 1
 
-#include <memory>
-#include <optional>
-#include <set>
-#include <string>
-#include <tuple>
+# include <memory>
+# include <optional>
+# include <set>
+# include <string>
+# include <tuple>
 
-#include <unistd.h>
+# include <unistd.h>
 
 
 namespace terminal {
@@ -56,10 +56,173 @@ namespace terminal {
 
 
   enum features {
-    col132,
-    printer,
+    decckm = 1,          // Cursor keys mode
+    devanm = 2,          // ANSI/VT52 mode
+    deccollm = 3,        // Column
+    decsclm = 4,         // Scrolling
+    decscnm = 5,         // Screen mode (light or dark screen)
+    decom = 6,           // Origin mode
+    decawm = 7,          // Auto wrap mode
+    decarm = 8,          // Auto repeat mode
+    decinlm = 9,         // Interlace mode
+    decedm = 10,         // Editing mode
+    decltm = 11,         // Line transmit mode
+    deckanam = 12,       // Katakana shift mode
+    decscfdm = 13,       // Space compressiion/field delimiter mode
+    dectem = 14,         // Transmit execution mode
+    decekem = 16,        // Edit key execution mode
+    decpff = 18,         // Print form feed
+    decpex = 19,         // Printer extend
+    ov1 = 20,            // Overstrike
+    ba1 = 21,            // Local BASIC
+    ba2 = 22,            // Host BASIC
+    pk1 = 23,            // Programmable keypad
+    ah1 = 24,            // Auto hardcopy
+    dectcem = 25,        // Text cursor enable mode
+    decpsp = 27,         // Proportional spacing
+    decpsm = 29,         // Pitch select mode
+    show_scrollbar = 30, // Show scrollbar (rxvt)
+    decrlm = 34,         // Cursor right to left mode
+    dechebm = 35,        // Hebrew (keyboard) mode
+    dechem = 36,         // Hebrew encoding mode
+    dectek = 38,         // Tektronix 4010/4014 mode
+    deccrnlm = 40,       // Carriage return/new line mode
+    devupm = 41,         // Unidirectional print mode
+    decnrcm = 42,        // National replacement character set mode
+    decgepm = 43,        // Graphics expanded print mode
+    decgpcm = 44,        // Graphics print color mode
+    decgpcs = 45,        // Graphics print color syntax
+    decgpbm = 46,        // Graphics print background mode
+    decgrpm = 47,        // Graphics rotated print mode
+    decthaim = 49,       // Thai input mode
+    decthaicm = 50,      // Thai cursor mode
+    decbwrm = 51,        // Black/white reversal mode
+    decopm = 52,         // Origin placement mode
+    dec131tm = 53,       // VT131 transmit mode
+    decbpm = 55,         // Bold page mode
+    decnakb = 57,        // Greek/N-A keyboard mapping mode
+    decipem = 58,        // Enter IBM proprinter emulation mode
+    deckkdm = 59,        // Kanji/katakana display mode
+    dechccm = 60,        // Horizontal cursor coupling mode
+    decvccm = 61,        // Vertical cursor coupling mode
+    decpccm = 64,        // Page cursor coupling mode
+    decbcmm = 65,        // Business color matching mode
+    decnkm = 66,         // Numeric keypad mode
+    decbkm = 67,         // Backarrow key mode
+    deckbum = 68,        // Keyboard usage mode
+    decvssm = 69,        // Vertical split screen mode
+    declrmm = 69,        // Left right margin mode
+    decfpm = 70,         // Force plot mode
+    decxrlm = 73,        // Transmission rate limiting
+    decsdm = 80,         // Sixel display mode
+    sixel = 80,
+    deckpm = 81,                      // Key position mode
+    wy52line = 83,                    // 52 line (WY-370)
+    wyenat = 84,                      // Erasable/nonerasable WNEAT off attribute select (WY-370)
+    replacement_color = 85,           // replacement character color (WY-370)
+    decthaiscm = 90,                  // That space compensating mode
+    decncsm = 95,                     // No clearing screen on column change mode
+    decrlcm = 96,                     // right to left copy mode
+    deccrtsm = 97,                    // CRT save mode
+    decarsm = 98,                     // Auti resize mode
+    decmcm = 99,                      // Modem control mode
+    decaam = 100,                     // Auto answerback mode
+    deccansm = 101,                   // Conceal answerback message mode
+    decnulm = 102,                    // Ignore null mode
+    dechdpxm = 103,                   // Half duplex mode
+    deceskm = 104,                    // Secondary keyboard language mode
+    decoscnm = 106,                   // Overscan mode
+    decnumlk = 108,                   // NumLock mode
+    deccapslk = 109,                  // Caps lock mode
+    decklhim = 110,                   // Keyboard LESs host indicator mode
+    decfwm = 111,                     // Framed window mode
+    decrpl = 112,                     // Review previous lines mode
+    dechwum = 113,                    // Host wake-up mode
+    decatcum = 114,                   // Alternate text color underline mode
+    decatcbm = 115,                   // Alternate text color blink mode
+    decbbsm = 116,                    // Bold and blink style mode
+    dececm = 117,                     // Erase color mode
+    mouse_event = 1000,               // Send mouse x&y on button press (xterm)
+    hilite_mouse = 1001,              // Use hilite mouse tracking (xterm)
+    cell_motion = 1002,               // Use cell motion mouse tracking (xterm)
+    all_motion = 1003,                // Use all motion mouse tracking (xterm)
+    focus_in_out = 1004,              // Send focusin/focusout events (xterm)
+    utf8_mouse = 1005,                // Enable UTF-8 mouse tracking (xterm)
+    sgr_mouse = 1006,                 // Enable SGR mouse mode (xterm)
+    alternate_scroll = 1007,          // Enable alternate scoll mode (xterm)
+    scroll_bottom_output = 1010,      // Scroll to bottom on tty output (rxvt)
+    scroll_bottom_key = 1011,         // Scroll to bottom on key press (rxvt)
+    fastscroll = 1014,                // Enable fastscroll resource (xterm)
+    urxvt_mouse = 1015,               // Enable urxvt mouse mode (urxvt)
+    sgr_pixel = 1016,                 // Enable SGR mouse pixelmode (xterm)
+    bold_high = 1021,                 // Bold/Italic implies high intensity (rxvt)
+    meta_key = 1034,                  // Interpret "meta" key (xterm)
+    alt_numlock_mod = 1035,           // Enable special modifiers for alt and numlock keys (xterm)
+    esc_meta = 1036,                  // Send ESC when meta modifies a key (xterm)
+    keypad_del = 1037,                // Send DEL from the editing-keypad delete key (xterm)
+    esc_alt = 1039,                   // Send ESC when alt modifies a key (xterm)
+    selection_not_highlighted = 1040, // Keep selection even if not highlighted (xterm)
+    clipboard_selection = 1041,       // Use the clipboard selection (xterm)
+    ctrlg_hint = 1042,                // Enable urgency window manager hint when control-G is received (xterm)
+    ctrlg_raise = 1043,               // Enable raising of the window when control-G is received (xterm)
+    reuse_clipboard = 1044,           // Reuse the most reven data copied to clipboard (xterm)
+    xtrevwrap2 = 1045,                // Extended reverse-wraparound mode (xterm)
+    switch_alt = 1046,                // Enable switching to/from alternate screen buffer (xterm)
+    use_alt = 1047,                   // Use alternate screen buffer (xterm)
+    cursor_save = 1048,               // Save cursor as in DECSC (xterm)
+    cursor_alt_save = 1049,           // Save cursor as in DECSC and use alternate screen buffer (xterm)
+    terminfo_fnkey = 1050,            // Set terminfo/termcap function-key mode (xterm)
+    sun_fnkey = 1051,                 // Set Sun function-key mode (xterm)
+    hp_fnkey = 1052,                  // Set HP function-key mode (xterm)
+    sco_fnkey = 1053,                 // Set SCO function-key mode (xterm)
+    legacy_keyboard = 1060,           // Set legacy keyboard emulation , i.e., X11R6 (xterm)
+    vt220_keyboard = 1061,            // Set VT220 keyboard emulation (xterm)
+    private_color = 1070,             // Use private color registers for each graphics (xterm)
+    arrow_key_swap = 1243,            // Arrow keys swapping (BiDi) (VTE)
+    key_up = 1337,                    // Report key up (iTerm2)
+    readline_mouse_1 = 2001,          // Enable readline mouse button-1 (xterm)
+    readline_mouse_2 = 2002,          // Enable readline mouse button-2 (xterm)
+    readline_mouse_3 = 2003,          // Enable readline mouse button-3 (xterm)
+    bracket_paste = 2004,             // Set bracketed paste mode (xterm)
+    char_quoting = 2005,              // Enable readline character-quoting (xterm)
+    newline_pasting = 2006,           // Enable readline newline pasting (xterm)
+    sync_output = 2026,               // Synchronized output (contour)
+    grapheme = 2028,                  // Grapheme cluster processing (contour)
+    passive_mouse = 2029,             // Passive mouse tracking (contour)
+    report_grid = 2030,               // Report grid cell selection (contour)
+    palette_updates = 2031,           // Color palette updates (contour)
+    mirror_box_drawing = 2500,        // Mirror box drawing characters (VTE)
+    bidi_auto = 2501,                 // BiDi autodetection (VTE)
+    report_ambiguous = 7700,          // Ambiguous width reporting (mintty)
+    scroll_markers = 7711,            // Scroll markers (prompt start) (mintty)
+    rewrap_resize = 7723,             // Rewrap in resize (mintty)
+    app_esc_key = 7727,               // Application escape key mode (mintty)
+    backslash_esc = 7728,             // Send ^\ instead of ^[ for ESC key (mintty)
+    graphics_pos = 7730,              // Graphics position (mintty)
+    alt_mode_wheel = 7765,            // Alt-modified mousewheel mode (mintty)
+    show_hide_scrollbar = 7766,       // Show/hide scrollbar (mintty)
+    font_change = 7767,               // Font change reporting (mintty)
+    shortcut_key = 7783,              // Shortcut key mode (mintty)
+    mousewheel = 7786,                // Mousewheel reporting (mintty)
+    app_mousewheel = 7787,            // Application mousewheel reporting (mintty)
+    current_bidi = 7796,              // BiDi in current line (mintty)
+
+    mouse_tracking = 20009,        // Mouse tracking (xterm)
+    show_toolbar = 20010,          // Show toolbar (rxvt)
+    blinking_cursor = 20012,       // Blinking cursor (xterm)
+    start_blinking_cursor = 20013, // Start blinking cursor (xterm)
+    xor_blinking = 20014,          // Enable XOR of blinking cursor control sequence and menu
+    font_shifting = 20035,         // Enable font-shifting functions (rxvt)
+    col132 = 20040,                // Allow 80 ⇒ 132 mode (xterm)
+    more_fix = 20041,              // more fix (xterm)
+    margin_bell = 20044,           // Turn on margin bell (xterm)
+    reverse_wrap = 20045,          // Reverse-wraparound mode (xterm)
+    start_logging = 20046,         // Start logging (xterm)
+    alternative_buffer = 20047,    // Use alternative scsreen buffer (xterm)
+
+    // XYZ Old features, merge
+    printer = 10000,
     regis,
-    sixel, // Sixel graphics
     selerase,
     drcs,
     udk,
